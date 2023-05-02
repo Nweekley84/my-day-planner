b2936bc85c58e244f4ac0328fb68bb844f969f9f
Nweekley84 <nweekley27@gmail.com>
2023-05-02 16:16:27 -0700

    Got advanced format plugin working, aded date stamp to header

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,13 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+
+// save reference to important DOM elements
+var timeDisplayEl = $('#current-time-display');
+
+
 $(function () {
+
     // TODO: Add a listener for click events on the save button. This code should
     // use the id in the containing time-block as a key to save the user input in
     // local storage. HINT: What does `this` reference in the click listener
@@ -20,5 +26,13 @@ $(function () {
     // attribute of each time-block be used to do this?
     //
     // TODO: Add code to display the current date in the header of the page.
+    // handle displaying the time
+function displayTime() {
+    var rightNow = dayjs().format('dddd, MMMM Do');
+    timeDisplayEl.text(rightNow);
+  }
+
+displayTime(); // show date
+setInterval(displayTime, 1000);
   });
   
